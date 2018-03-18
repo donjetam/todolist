76667443c30345a2e40d44c108541be50bec316a
unknown <egzona-g@hotmail.com>
2018-03-18 23:19:58 +0100

    Adding main prject files

---
+++ TDL/Models/TDLViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDL.Models
+{
+    public class TDLViewModel
+    {
+        public int tid { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public Nullable<bool> IsDeleted { get; set; }
+        public Nullable<int> id { get; set; }
+
+        public string dname { get; set; }
+    }
+}
---
+++ TDL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TDL.Startup))]
+namespace TDL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
